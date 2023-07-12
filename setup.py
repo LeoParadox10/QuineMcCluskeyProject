43a27206038d000ae265c527a187b648acfe7340
Smarak Patra <43983935+LeoParadox10@users.noreply.github.com>
2023-07-12 12:01:44 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      license = "MIT License",
+      description='Python implementation of quine mccluskey algorithm',
+      author='Smarak Patra',
+      author_email='smarakpatra2711@gmail.com',
+      url='',
+      packages=['qm' ,'tests'],
+      long_description=open('README.md').read(),
+     )
+
